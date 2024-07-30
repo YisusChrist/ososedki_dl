29199291855af3a4b2d6bff00dad773ef03a36e0
YisusChrist <alejandro.gonzalezm02@estudiante.uam.es>
2024-07-30 19:08:33 +0200

    feat(fapello_is): add more output information on scrapping

--- ososedki_dl/crawlers/fapello_is.py
+++ ososedki_dl/crawlers/fapello_is.py
@@ -2,7 +2,6 @@
 
 from pathlib import Path
 
-import requests  # type: ignore
 from aiohttp import ClientSession
 from rich import print
 from rich.progress import Progress, TaskID
@@ -11,6 +10,13 @@ from ososedki_dl.crawlers._common import download_media_items
 from ososedki_dl.utils import get_final_path
 
 BASE_URL = "https://fapello.is//api/media"
+
+
+async def fetch_media_urls(session: ClientSession, url: str) -> list[dict] | str:
+    async with session.get(url) as response:
+        if response.status != 200:
+            return []
+        return await response.json()
 
 
 async def download_profile(
@@ -28,18 +34,23 @@ async def download_profile(
 
     title: str = ""
     urls: list = []
+    print(f"Fetching data from profile {profile_id}...")
     while True:
         url: str = f"{BASE_URL}/{profile_id}/{i}/1/0"
-        print("Sending request to:", url)
-        response: requests.Response = requests.get(url)
-        if response.text == "null":
+        album: list[dict] | str = await fetch_media_urls(session, url)
+        if not album or album == "null":
+            break
+
+        if isinstance(album, str):
+            print("Error fetching album:", album)
             break
 
         if not title:
-            title = response.json()[0]["name"]
-        album: list[dict] = response.json()
+            title = album[0]["name"]
         urls += [media["newUrl"] for media in album if media["newUrl"]]
         i += 1
+
+    print(f"Found {len(urls)} media items in profile {profile_id}")
 
     album_path: Path = get_final_path(download_path, title)
 
