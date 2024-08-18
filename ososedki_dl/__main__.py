ca4793c9bc9b52e81a40dc63bc3109651e37cef8
YisusChrist <alejandro.gonzalezm02@estudiante.uam.es>
2024-08-18 11:42:26 +0200

    feat: simplify the scrapping module, remove individual scrapper function

--- ososedki_dl/__main__.py
+++ ososedki_dl/__main__.py
@@ -8,27 +8,18 @@ from rich.traceback import install
 
 from .cli import exit_session, get_parsed_args
 from .consts import EXIT_SUCCESS
-from .scrapper import downloaders, generic_download
+from .scrapper import generic_download
 from .utils import get_user_input
 
 
 async def run_main_loop() -> None:
     async with ClientSession() as session:
         while True:
-            url, download_path = get_user_input()
+            urls, download_path = get_user_input()
 
-            # Find the appropriate downloader based on the URL
-            for domain, downloader in downloaders.items():
-                if url.startswith(domain):
-                    await generic_download(
-                        session=session,
-                        urls=[url],
-                        download_path=download_path,
-                        download_func=downloader,
-                    )
-                    break
-            else:
-                print(f"No downloader found for URL: {url}")
+            await generic_download(
+                session=session, urls=urls, download_path=download_path
+            )
 
 
 def main() -> None:
--- ososedki_dl/scrapper.py
+++ ososedki_dl/scrapper.py
@@ -2,12 +2,46 @@
 
 from pathlib import Path
 from typing import Awaitable, Callable
+from importlib import import_module
 
 from aiohttp import ClientSession
 from rich.console import Console
 from rich.progress import Progress, TaskID
 
 console = Console()
+
+
+# Mapping of domain patterns to their respective module and function names
+downloaders: dict[str, tuple[str, str]] = {
+    "https://bunkr-albums.io": (
+        "ososedki_dl.crawlers.bunkrr_albums",
+        "find_albums",
+    ),
+    "https://eromexxx.com": (
+        "ososedki_dl.crawlers.eromexxx",
+        "download_profile",
+    ),
+    "https://fapello.is": (
+        "ososedki_dl.crawlers.fapello_is",
+        "download_profile",
+    ),
+    "https://husvjjal.blogspot.com": (
+        "ososedki_dl.crawlers.husvjjal_blogspot",
+        "download_profile",
+    ),
+    "https://ososedki.com": (
+        "ososedki_dl.crawlers.ososedki",
+        "download_album",
+    ),
+    "https://sorrymother.to": (
+        "ososedki_dl.crawlers.sorrymother",
+        "download_album",
+    ),
+    "https://wildskirts.com": (
+        "ososedki_dl.crawlers.wildskirts",
+        "download_profile",
+    ),
+}
 
 
 def print_errors(results: list[dict[str, str]]) -> None:
@@ -18,22 +52,21 @@ def print_errors(results: list[dict[str, str]]) -> None:
 
 
 async def generic_download(
-    session: ClientSession,
-    urls: list[str],
-    download_path: Path,
-    download_func: Callable[
-        [ClientSession, str, Path, Progress, TaskID], Awaitable[list[dict[str, str]]]
-    ],
+    session: ClientSession, urls: list[str], download_path: Path
 ) -> None:
     with Progress() as progress:
         task: TaskID = progress.add_task("[cyan]Downloading...", total=len(urls))
 
         results: list[dict[str, str]] = []
         for url in urls:
-            result: list[dict[str, str]] = await download_func(
-                session, url, download_path, progress, task
+            await handle_downloader(
+                session=session,
+                download_path=download_path,
+                progress=progress,
+                task=task,
+                results=results,
+                url=url,
             )
-            results.extend(result)
 
         ok_count: int = sum(1 for result in results if result["status"] == "ok")
         skipped_count: int = sum(
@@ -47,127 +80,51 @@ async def generic_download(
 [yellow]Skipped: {skipped_count}[/]
 [red]Errors: {error_count}[/]\n"""
         )
+
         if error_count > 0:
             print_errors(results)
 
 
-async def _generic_callback(
-    callback: Callable[
-        [ClientSession, str, Path, Progress, TaskID], Awaitable[list[dict[str, str]]]
-    ],
+async def handle_downloader(
     session: ClientSession,
-    url: str,
     download_path: Path,
     progress: Progress,
     task: TaskID,
+    results: list[dict[str, str]],
+    url: str,
+) -> None:
+    for pattern, (module_name, function_name) in downloaders.items():
+        if url.startswith(pattern):
+            result: list[dict[str, str]] = await dynamic_download(
+                session=session,
+                album_url=url,
+                download_path=download_path,
+                progress=progress,
+                task=task,
+                module_name=module_name,
+                function_name=function_name,
+            )
+            results.extend(result)
+            break
+    else:
+        console.print(f"[yellow]No downloader found for URL: {url}[/]")
+
+
+async def dynamic_download(
+    session: ClientSession,
+    album_url: str,
+    download_path: Path,
+    progress: Progress,
+    task: TaskID,
+    module_name: str,
+    function_name: str,
 ) -> list[dict[str, str]]:
-    result: list[dict[str, str]] = await callback(
-        session, url, download_path, progress, task
+    module = import_module(module_name)
+    download_func = getattr(module, function_name)
+
+    result: list[dict[str, str]] = await download_func(
+        session, album_url, download_path, progress, task
     )
+
     progress.advance(task)
     return result
-
-
-async def eromexxx_download(
-    session: ClientSession,
-    album_url: str,
-    download_path: Path,
-    progress: Progress,
-    task: TaskID,
-) -> list[dict[str, str]]:
-    from ososedki_dl.crawlers.eromexxx import download_profile
-
-    return await _generic_callback(
-        download_profile,
-        session,
-        album_url,
-        download_path,
-        progress,
-        task,
-    )
-
-
-async def ososedki_download(
-    session: ClientSession,
-    album_url: str,
-    download_path: Path,
-    progress: Progress,
-    task: TaskID,
-) -> list[dict[str, str]]:
-    from ososedki_dl.crawlers.ososedki import download_album
-
-    return await _generic_callback(
-        download_album,
-        session,
-        album_url,
-        download_path,
-        progress,
-        task,
-    )
-
-
-async def sorrymother_download(
-    session: ClientSession,
-    album_url: str,
-    download_path: Path,
-    progress: Progress,
-    task: TaskID,
-) -> list[dict[str, str]]:
-    from ososedki_dl.crawlers.sorrymother import download_album
-
-    return await _generic_callback(
-        download_album,
-        session,
-        album_url,
-        download_path,
-        progress,
-        task,
-    )
-
-
-async def wildskirts_download(
-    session: ClientSession,
-    album_url: str,
-    download_path: Path,
-    progress: Progress,
-    task: TaskID,
-) -> list[dict[str, str]]:
-    from ososedki_dl.crawlers.wildskirts import download_profile
-
-    return await _generic_callback(
-        download_profile,
-        session,
-        album_url,
-        download_path,
-        progress,
-        task,
-    )
-
-
-async def fapello_is_download(
-    session: ClientSession,
-    album_url: str,
-    download_path: Path,
-    progress: Progress,
-    task: TaskID,
-) -> list[dict[str, str]]:
-    from ososedki_dl.crawlers.fapello_is import download_profile
-
-    return await _generic_callback(
-        download_profile,
-        session,
-        album_url,
-        download_path,
-        progress,
-        task,
-    )
-
-
-# Mapping of domain patterns to their respective download functions
-downloaders: dict[str, Callable] = {
-    "https://eromexxx.com": eromexxx_download,
-    "https://fapello.is": fapello_is_download,
-    "https://ososedki.com": ososedki_download,
-    "https://sorrymother.to": sorrymother_download,
-    "https://wildskirts.com": wildskirts_download,
-}
