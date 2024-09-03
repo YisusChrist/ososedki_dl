e2159b10eec052a45a3a9ad1169cd00e5ea774ba
YisusChrist <agelrenorenardo@gmail.com>
2024-09-04 01:01:15 +0200

    feat(waifubitches): add support for model page scrapping

--- ososedki_dl/crawlers/waifubitches.py
+++ ososedki_dl/crawlers/waifubitches.py
@@ -1,12 +1,15 @@
 """Downloader for https://waifubitches.com"""
 
+import asyncio
 from pathlib import Path
+from time import sleep
 
 from aiohttp import ClientSession
 from bs4 import BeautifulSoup  # type: ignore
 from rich.progress import Progress, TaskID
 
-from ososedki_dl.crawlers._common import (process_album, search_ososedki_media,
+from ososedki_dl.crawlers._common import (fetch_soup, process_album,
+                                          search_ososedki_media,
                                           search_ososedki_title)
 from ososedki_dl.utils import main_entry
 
@@ -22,6 +25,55 @@ def waifubitches_media_filter(soup: BeautifulSoup) -> list[str]:
     return search_ososedki_media(soup=soup, base_url=BASE_URL)
 
 
+async def fetch_page_albums(session: ClientSession, page_url: str) -> list[str]:
+    soup: BeautifulSoup | None = await fetch_soup(session, page_url)
+    if not soup:
+        return []
+
+    # Find all links that start with https://waifubitches.com/gallery/
+    albums: list[str] = [
+        f"{DOWNLOAD_URL}{a["href"]}"
+        for a in soup.find_all(
+            "a", href=lambda x: x and x.startswith("/gallery/")
+        )
+    ]
+    albums = list(set(albums))
+
+    return albums
+
+async def find_model_albums(
+    session: ClientSession, model_url: str
+) -> tuple[list[str], str]:
+    # Clean the URL removing the query parameters
+    model_url = model_url.split("?")[0]
+
+    soup: BeautifulSoup | None = await fetch_soup(session, model_url)
+    if not soup:
+        return [], ""
+    model_name: str = get_model_name(soup)
+
+    albums: list[str] = []
+    albums_found = True
+    i = 1
+
+    while albums_found:
+        page_url: str = f"{model_url}?page={i}"
+        albums_extracted: list[str] = await fetch_page_albums(session, page_url)
+        if not albums_extracted:
+            albums_found = False
+            break
+
+        albums.extend(albums_extracted)
+        i += 1
+        sleep(0.5)
+
+    return albums, model_name
+
+
+def get_model_name(soup: BeautifulSoup) -> str:
+    return soup.find("title").text.split(" nude")[0].strip()
+
+
 @main_entry
 async def download_album(
     session: ClientSession,
@@ -30,6 +82,27 @@ async def download_album(
     progress: Progress,
     task: TaskID,
 ) -> list[dict[str, str]]:
+    if album_url.startswith(f"{DOWNLOAD_URL}/model/"):
+        # Find all the albums for the model
+        albums, model = await find_model_albums(session, album_url)
+
+        tasks: list = [
+            process_album(
+                session=session,
+                album_url=album,
+                download_path=download_path,
+                progress=progress,
+                task=task,
+                media_filter=waifubitches_media_filter,
+                title=model,
+            )
+            for album in albums
+        ]
+
+        results: list[dict[str, str]] = await asyncio.gather(*tasks)
+        return results
+
+
     return await process_album(
         session=session,
         album_url=album_url,
