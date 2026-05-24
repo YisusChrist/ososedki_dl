8a1f85ebf098acc4f3b4013a737cffe1b6f36641
YisusChrist <agelrenorenardo@gmail.com>
2026-05-24 17:16:11 +0200

    refactor(crawlers): move cosxuxi related crawlers to a separate folder

--- ososedki_dl/crawlers/other/__init__.py
+++ ososedki_dl/crawlers/other/__init__.py
@@ -1,21 +1,15 @@
 from .bunkrr_albums import BunkrAlbumsCrawler
-from .cosxuxi_club import CosxuxiClubCrawler
 from .eromexxx import EromeXXXCrawler
 from .fapello_is import FapelloIsCrawler
 from .husvjjal_blogspot import HusvjjalBlogspotCrawler
-from .kaizty import KaiztyCrawler
-from .nungvl import NungvlCrawler
 from .sorrymother import SorryMotherCrawler
 from .wildskirts import WildskirtsCrawler
 
 crawlers = [
     BunkrAlbumsCrawler,
-    CosxuxiClubCrawler,
     EromeXXXCrawler,
     FapelloIsCrawler,
     HusvjjalBlogspotCrawler,
-    KaiztyCrawler,
-    NungvlCrawler,
     SorryMotherCrawler,
     WildskirtsCrawler,
 ]
